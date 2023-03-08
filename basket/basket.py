f1767eb2acacb27f4b5228d7e53351f30321d439
AlifaghaSalmanov <alifaghasalmanov@gmail.com>
2023-03-08 17:54:38 +0400

    basket payment calculation

--- basket/basket.py
+++ basket/basket.py
@@ -9,9 +9,9 @@ class Basket:
 
     def __init__(self, request):
         self.session = request.session
-        basket = self.session.get("skey")
-        if "skey" not in request.session:
-            basket = self.session["skey"] = {}
+        basket = self.session.get(settings.BASKET_SESSION_ID)
+        if settings.BASKET_SESSION_ID not in request.session:
+            basket = self.session[settings.BASKET_SESSION_ID] = {}
         self.basket = basket
 
     def add(self, product, qty):
@@ -43,10 +43,21 @@ class Basket:
         """Get the basket data and count the qty of items"""
         return sum(item["qty"] for item in self.basket.values())
 
-    def get_total_price(self):
+    def get_subtotal_price(self):
         return sum(
             Decimal(item["price"]) * item["qty"] for item in self.basket.values()
         )
+
+    def get_total_price(self):
+        subtotal = sum(
+            Decimal(item["price"]) * item["qty"] for item in self.basket.values()
+        )
+        if subtotal == 0:
+            shipping = Decimal(0.00)
+        else:
+            shipping = Decimal(11.50)
+        total = subtotal + Decimal(shipping)
+        return total
 
     def delete(self, product_id):
         """Delete item from session data
@@ -73,7 +84,7 @@ class Basket:
 
     def clear(self):
         # Remove basket from session
-        del self.session["skey"]
+        del self.session[settings.BASKET_SESSION_ID]
         self.save()
 
     def save(self):
